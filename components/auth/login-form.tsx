3dbdeeafe576e1c9c8b13a1548fcb2351eca64f8
ajay-mandal <ajayrox48@gmail.com>
2024-04-12 16:13:06 +0530

    structure added

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from 'zod';
+import { LoginSchema } from "@/schemas";
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form";
+import { CardWrapper } from "@/components/auth/card-wrapper";
+
+export function LoginForm() {
+
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+            email:"",
+            password:"",
+        },
+    });
+
+    return(
+        <CardWrapper
+        headerLabel="Welcome back"
+        backButtonLabel="Don't have an account?"
+        backButtonHref="/register"
+        showSocial
+        >
+            Login Form!
+        </CardWrapper>
+    )
+}
