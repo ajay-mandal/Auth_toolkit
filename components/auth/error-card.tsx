be9d5f6fc8b39b3dd23cb42c30684cca7851778d
ajay-mandal <ajayrox48@gmail.com>
2024-04-14 19:58:27 +0530

    error card saturated

--- components/auth/error-card.tsx
+++ components/auth/error-card.tsx
@@ -1,24 +1,16 @@
-import { Header } from "@/components/auth/header";
-import BackButton from "@/components/auth/backbutton";
-import {
-    Card,
-    CardFooter,
-    CardHeader
-} from "@/components/ui/card"
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
 
 export const ErrorCard = () => {
     return(
-        <Card className="w-[400px] shadow-md">
-            <CardHeader>
-                <Header label="Oops! Something went wrong!" />
-            </CardHeader>
-            <CardFooter>
-                <BackButton
-                label="Back to login"
-                href="/login"
-                />
-            </CardFooter>
-
-        </Card>
+        <CardWrapper
+        headerLabel="Oops! Something went wrong!"
+        backButtonHref="/login"
+        backButtonLabel="Back to login"
+        >
+            <div className="w-full flex justify-center items-center">
+                <ExclamationTriangleIcon className="text-destructive w-6 h-6" />
+            </div>
+        </CardWrapper>
     )
 }
