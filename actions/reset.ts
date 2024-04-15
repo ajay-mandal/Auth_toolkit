e986654fe8227e64c55e48fb6a04bbc2323a91e7
ajay-mandal <ajayrox48@gmail.com>
2024-04-15 12:15:34 +0530

    reset form in progress

---
+++ actions/reset.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import * as z from "zod";
+
+import { ResetSchema } from "@/zod/validator";
+import { getUserByEmail } from "@/data/user";
+
+export const reset = async (values: z.infer<typeof ResetSchema>) => {
+    const validatedFields = ResetSchema.safeParse(values);
+    if(!validatedFields.success) {
+        return { error : "Invalid email!" };
+    }
+    const { email } = validatedFields.data;
+
+    const existingUser = await getUserByEmail(email);
+
+    if(!existingUser) {
+        return { error: "Email not found!" };
+    }
+
+    // TODO: Generate token and send email
+
+    return { success: "Reset email sent!" };
+}
